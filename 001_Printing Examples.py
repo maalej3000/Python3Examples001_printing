09e69dac7723181b9a6b2fa495678308f48273ed
maalej3000 <m.maalej@gmail.com>
2020-08-26 21:57:45 +0300

    001_Printing Examples.py

---
+++ 001_Printing Examples.py
@@ -0,0 +1,5 @@
+print("hello World")   #You can use quote(") or single quote(') 
+print('hello World')
+
+print("hello", " World") #Concatenate two strings with comma(,) or plus(+)
+print("hello", " World")
